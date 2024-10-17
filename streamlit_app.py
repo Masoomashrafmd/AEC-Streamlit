70866027ca9aada8dd050616810949618c67ab77
Masoomashrafmd <132615555+Masoomashrafmd@users.noreply.github.com>
2024-10-17 09:58:54 +0000

    nothing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title(" My forgex app")
+st.title(" forgex app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
